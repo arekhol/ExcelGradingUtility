78d0e44f84d4eb0f5c856c14f29017c21ec50812
Ari Ekholm <ari.ekholm@samk.fi>
2025-04-03 18:57:44 +0300

    Added all files in the directory

---
+++ push.bat
@@ -0,0 +1,8 @@
+git add .
+git remote add origin https://github.com/arekhol/ExcelGradingUtility.git
+git branch -M main
+git push -u origin main
+git commit -m "Added all files in the directory"
+git ls-files
+git remote add origin https://github.com/arekhol/ExcelGradingUtility.git
+git push -u origin main
